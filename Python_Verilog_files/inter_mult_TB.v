e6efe19b48f7f676b758b134995f871e0adc69b0
viki22052 <35682409+viki22052@users.noreply.github.com>
2021-05-13 12:08:09 +0300

    Updated Testbench

--- Python_Verilog_files/inter_mult_TB.v
+++ Python_Verilog_files/inter_mult_TB.v
@@ -3,15 +3,17 @@ parameter n = 2;
 reg [n-1:0] address;
  reg read_en;
  wire [n-1:0] data;
- integer i;
+ integer i, j;
  
  initial begin
    address = 0;
    read_en = 0;
    #10 $monitor ("address = %b, data = %b, read_en = %b", address, data, read_en);
-   for (i = 0; i <(2**n); i = i +1 )begin
-     #10 address = i;
+   for (i = 0; i <(2**n); i = i +1 ) begin
+    for (j = 0; j<(2**n); j = j + 1) begin
+     #10 address = {i ,j};
      read_en = 1;
+   end
    end
  end
  
